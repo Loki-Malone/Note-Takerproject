5884745147c7b7fb4de905054bb1e4592a2b4b5a
Loki-Malone <60401309+Loki-Malone@users.noreply.github.com>
2020-04-23 21:27:16 -0500

    removed handlerequest sever cause was found not needed

--- server.js
+++ server.js
@@ -3,15 +3,7 @@ var fs = require("fs")
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-function handleRequest(req, res) {
 
-  // Here we use the fs package to read our index.html file
-  fs.readFile(__dirname + "/index.html", function(err, data) {
-    if (err) throw err;
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  });
-}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
