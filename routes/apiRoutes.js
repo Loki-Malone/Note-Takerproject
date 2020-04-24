c35c76fbf7d7de476f88257ca81a6dc12f845d2f
Loki-Malone <60401309+Loki-Malone@users.noreply.github.com>
2020-04-23 21:07:59 -0500

    created app.gets for api save, delete, and write data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,20 @@
 var deleteData = require("../data/deleteData");
 var saveData = require("../data/saveData");
 var writeData = require("../data/writeData");
+
+module.exports = function(app) {
+    
+  
+    app.get("/api/delete", function(req, res) {
+      res.json(deleteData);
+    });
+  
+    app.get("/api/save", function(req, res) {
+      res.json(saveData);
+    });
+
+    app.get("/api/write", function(req, res) {
+        res.json(writeData);
+      });
+
+};
